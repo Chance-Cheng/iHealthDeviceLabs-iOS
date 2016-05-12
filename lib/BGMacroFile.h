516100c8130d5860b2c3f08cf0110e73bb6c4d56
iHealthLabs <jingzhiwei@ihealthlabs.com.cn>
2016-05-12 16:09:15 +0800

    Add BG errorID description

--- lib/BGMacroFile.h
+++ lib/BGMacroFile.h
@@ -39,6 +39,27 @@ typedef void (^DisposeBGResultBlock)(NSDictionary* result);
 typedef void (^DisposeBGSendCodeBlock)(BOOL sendOk);
 //
 typedef void (^DisposeBGErrorBlock)(NSNumber* errorID);
+/*
+ errorID:
+ 00：Battery is low.
+ 01：Glucose test result is out of the measurement range.
+ 02：Unknown interference detected, please repeat the test.
+ 03：Strip is used or unknown moisture detected, discard the test strip and repeat the test with a new strip.
+ 04：Reading transmission error. Repeat the test with a new test strip. If the problem persists, contact iHealth customer service for assistance.
+ 05\06：The environmental temperature is beyond normal range, place the meter at room temperature for at least 30 minutes, then repeat the test.
+ 07：Test strip coding error.
+ 08：Communication error, press“START” or rescan the code to repeat the test.
+ 09：Strip removed in the middle of reading, repeat the test with a new strip.
+ 10: Insert a new test strip and repeat the test.
+ 11: Cannot write to SN or KEY.
+ 12: Please set time.
+ 13: 0 test strips remaining.
+ 14: Test strip expired.
+ 15: Unplug the charging cable before testing.
+ 100: BG meter disconnected.
+ 101: BG meter is in sleeping mode, needs repair.
+ 111: user verification failed.
+*/
 //
 typedef void (^DisposeBGBottleID)(NSNumber *bottleID);
 //
